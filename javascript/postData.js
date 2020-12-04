d3b8cedd90e3c0f1918b320f789e8cc9a20720b8
NJohnQA <73649014+NJohnQA@users.noreply.github.com>
2020-12-04 14:33:33 +0000

    NJ: adding updated js

--- javascript/postData.js
+++ javascript/postData.js
@@ -25,6 +25,62 @@ function getData() {
 
 getData();
 
+const params = new URLSearchParams(window.location.search);
+
+for(let param of params ){
+    console.log("here i am",param)
+    let id = param[1];
+    console.log(id);
+    getData(id)
+}
+
+
+
+function getData(id) {
+    //for (let i = 0; i < 200; i++) {
+        fetch('https://petstore.swagger.io/v2/pet/' + id)
+            .then(
+                function (response) {
+                    if (response.status !== 200) {
+                        console.log('Looks like there was a problem. Status Code: ' +
+                            response.status);
+                        return;
+                    }
+                    // Examine the text in the response
+                    response.json().then(function (data) {
+                        console.log("MY DATA OBJ", data)
+
+                        document.querySelector("input#commentPet").value = data.postId
+                        document.querySelector("input#commentId").value = data.id
+                        document.querySelector("input#commentStatus").value = data.name
+
+                    });
+                }
+            )
+            .catch(function (err) {
+                console.log('Fetch Error :-S', err);
+            });
+    //}
+}
+
+document
+    .querySelector("form.viewRecord")
+    .addEventListener("submit", function (stop) {
+        stop.preventDefault();
+        let formElements = document.querySelector("form.viewRecord").elements;
+        console.log(formElements)
+        let pet = formElements["commentPet"].value;
+        let petId = formElements["input#commentId"].value;
+        let status = formElements["commentStatus"].value;
+
+        let data = {
+            "Pet": pet,
+            "ID": id,
+            "Status": status,
+        }
+        console.log("Data to post", data)
+    });
+
 function createPara(data) {
   for (let i = 0; i < 1; i++) {
       let id = data.id;
@@ -37,3 +93,18 @@ function createPara(data) {
       myDiv.appendChild(para);
   }
 }
+
+window.addEventListener('load', function() {
+    document.querySelector('input[type="file"]').addEventListener('change', function() {
+        if (this.files && this.files[0]) {
+            var img = document.querySelector('img');  // $('img')[0]
+            img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+            img.onload = imageIsLoaded;
+        }
+    });
+  });
+  
+  function imageIsLoaded() { 
+    alert(this.src);  // blob url
+    // update width and height ...
+  }
